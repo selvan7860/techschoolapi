acb35b41181b4420be0b2213d457e4149a128be1
lakshmanaselvan <lakshmanaselvan@lakshmanaselvans-MacBook-Air.local>
2024-07-20 13:41:29 +0530

    first_commit

---
+++ src/main/java/com/example/techschool/TechschoolApplication.java
@@ -0,0 +1,16 @@
+package com.example.techschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechschoolApplication.class, args);
+		System.out.println("***********************************************");
+		System.out.println("TECH SCHOOL PROJECT API STARTED SUCCESSFULLY");
+		System.out.println("***********************************************");
+	}
+
+}
---
+++ src/test/java/com/example/techschool/TechschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.techschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechschoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
